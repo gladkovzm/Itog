07b198ac2b8d381d89c8290e7f86197e687656ae
gladkov <gladkovzm@gmail.com>
2022-10-04 00:40:23 +0300

    Файл с программой готов

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+string[] first;
+Console.WriteLine("Введите массив через пробел, чтобы вывести массив с 3мя и менее символами");
+string prob = Console.ReadLine();
+first = prob.Split(' ');
+var result = new string[first .Length];
+var realSize = 0;
+
+foreach (var arr in first )
+{
+    if (arr.Length <= 3)
+    {
+        result[realSize] = arr;
+        realSize++;
+    }
+}
+PrintArray(result);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+            					
